6b48768c6fdd30fef750813eb0470e42fb7003df
Pragnesh <pragnesh007@gmail.com>
2023-07-21 10:41:49 +0530

    this is SampleDataSendActivity

--- app/src/main/java/com/example/sampleactivity/MainActivity.java
+++ app/src/main/java/com/example/sampleactivity/MainActivity.java
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
-import android.text.InputFilter;
-import android.text.Spanned;
 import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
@@ -20,7 +18,7 @@ import com.example.sampleactivity.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
-   // For use of the TextWatcher
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
